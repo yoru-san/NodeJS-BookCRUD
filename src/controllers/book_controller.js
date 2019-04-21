8f058ae7beb7a9a30fa7fa3b3f75472b1547fe6d
yoru-san <cecel31@free.fr>
2019-04-21 02:16:19 +0200

    ajout simultanée d'objet json dans un seul fichier

--- src/controllers/book_controller.js
+++ src/controllers/book_controller.js
@@ -1,6 +1,9 @@
 var Book = require('../models/book');
-const fs = require('fs')
+const fs = require('fs');
+const readFilePromise = require('fs-readfile-promise');
 
+
+var inMemory_books = [];
 
 exports.index = (_, res) => {
     fs.readFile('data/books.json', 'utf8', (err, jsonString) => {
@@ -20,36 +23,68 @@ exports.index = (_, res) => {
 
 }
 exports.show = (req, res) => {
-
+    var id = req.params;
+    console.log(id);
+    var book = inMemory_books.find(x => x.id === id);
+    res.json(book);
 }
 
 exports.create = (req, res) => {
-    var book = new Book();
-    const book_data = req.body;
-    console.log(req);
-    book.id = 4;
-    book.title = book_data.title;
-    book.author = book_data.author;
-    book.summary = book_data.summary;
-    book.type = book_data.type;
-    book.publication_date = book_data.publication_date;
 
-    const jsonString = JSON.stringify(book);
-    fs.writeFile('data/new_books.json', jsonString, err => {
-        var message;
-        if (err) {
-            message = "Error writing file: " +  err;
-        } else {
-            message = 'Successfully wrote file';
+    readFilePromise('data/books.json', 'utf-8').then((jsonString) => {
+        try {
+            return JSON.parse(jsonString)
+        } catch (err) {
+            console.log('Error parsing JSON string:', err)
+        }
+    }).then((old_book) => {
+        var book = new Book();
+        const book_data = req.body;
+        book.id = book_data.id;
+        book.title = book_data.title;
+        book.author = book_data.author;
+        book.summary = book_data.summary;
+        book.type = book_data.type;
+        book.publication_date = book_data.publication_date;
+
+        inMemory_books.push(old_book, book);
+
+        var json_books = [];
+        for (let i = 0; i < inMemory_books.length; i++) {
+            json_books.push(JSON.stringify(inMemory_books[i]));
         }
 
-        res.json(message);
-    })
+        var data = json_books.join(',');
+        data = "[" + data + "]";
 
+        fs.writeFile('data/new_books.json', data, err => {
+            var message;
+            if (err) {
+                message = "Error writing file: " + err;
+            } else {
+                message = 'Successfully wrote file';
+            }
+
+            res.json(message);
+        })
+    }).catch((err) => {
+        console.log("Error reading file from disk:", err)
+        return
+    });
 }
 
-exports.update = (req, res) => {
 
+exports.update = (req, res) => {
+    fs.readFile('data/new_books.json', (err, customer) => {
+        if (err) {
+            console.log('Error reading file:', err)
+            return
+        }
+
+        fs.writeFile('data/new_books.json', JSON.stringify(customer), (err) => {
+            if (err) console.log('Error writing file:', err)
+        })
+    })
 }
 
 exports.delete = (req, res) => {
