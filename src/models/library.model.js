b5ea609717523fe5c7324575ec8b4ec890ee5616
yoru-san <cecel31@free.fr>
2019-05-06 22:53:35 +0200

    retrait com

--- src/models/library.model.js
+++ src/models/library.model.js
@@ -1,22 +1,21 @@
 const mongoose = require('mongoose');
-const ObjectId = require('mongoose').Schema.Types.ObjectId;
 
 const schema = mongoose.Schema({
     name: { type: String, required: true },
     location: {
         type: {
-          type: String, // Don't do `{ location: { type: String } }`
-          enum: ['Point'], // 'location.type' must be 'Point'
-          required: true
+            type: String,
+            enum: ['Point'],
+            required: true
         },
         coordinates: {
-          type: [Number],
-          required: true
+            type: [Number],
+            required: true
         }
     },
 });
 
-schema.index({location: '2dsphere'});
+schema.index({ location: '2dsphere' });
 
 const model = mongoose.model('Library', schema);
 module.exports = model;
